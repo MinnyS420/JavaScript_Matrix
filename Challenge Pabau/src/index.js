4322c72ac4f2a1c6a560c7897b7531eb2cdc971f
mstamenkovski27@gmail.com <mstamenkovski27@gmail.com>
2024-08-06 11:57:56 +0200

    matrixMaze_JS

---
+++ Challenge Pabau/src/index.js
@@ -0,0 +1,101 @@
+"use strict";
+/**
+ * Awesome challenge I like it took me a night to think about it but I was able to figure it out.
+ * I have a few explanations here. I don't know if it's part of the challenge, but there was no '+' at the letter 'C',
+ * so the output is different.
+ *
+ * I added 2 more arrays so it can be from point A to point B or from '>' to 's' :D
+ * Have a nice day and happy coding :D
+ *
+ * Martin
+ */
+
+const matrix = [
+    ['>', '-', '-', '-', 'A', '-', '@', '-', '+'],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'],
+    [' ', '+', '-', 'U', '-', '+', ' ', ' ', 'C'],
+    [' ', '|', ' ', ' ', ' ', '|', ' ', ' ', '|'],
+    [' ', 's', ' ', ' ', ' ', 'C', '-', '-', '+'],
+];
+
+
+// Uncomment one of the following matrices if needed
+// const matrix = [
+//     ['>', '-', '-', '-', 'A', '-', '@', '-', '+'],
+//     [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'],
+//     [' ', '+', '-', 'U', '-', '+', ' ', ' ', 'C'],
+//     [' ', '|', ' ', ' ', ' ', '|', ' ', ' ', '|'],
+//     [' ', '|', ' ', ' ', ' ', 'C', ' ', ' ', '|'],
+//     [' ', '|', ' ', ' ', ' ', '|', ' ', ' ', '|'],
+//     [' ', 's', ' ', ' ', ' ', '+', '-', '-', '+'],
+// ];
+
+
+// const matrix = [
+//     ['>', '-', '-', '-', 'P', '-', '@', '-', '+'],
+//     [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|'],
+//     [' ', '+', '-', 'A', '-', '+', ' ', ' ', 'A'],
+//     [' ', '|', ' ', ' ', ' ', '|', ' ', ' ', '|'],
+//     [' ', 'U', ' ', ' ', ' ', 'B', ' ', ' ', '|'],
+//     [' ', '|', ' ', ' ', ' ', '|', ' ', ' ', '|'],
+//     [' ', 's', ' ', ' ', ' ', '+', '-', '-', '+'],
+// ];
+
+
+let path = '';
+let letters = '';
+const directions = {
+    right: [0, 1],
+    left: [0, -1],
+    up: [-1, 0],
+    down: [1, 0],
+};
+// Function to find the starting point and begin traversal
+function findStart(matrix) {
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] === '>') {
+                return [i, j];
+            }
+        }
+    }
+    return null;
+}
+// Function to traverse the matrix
+function traverseMatrix(matrix) {
+    const start = findStart(matrix);
+    if (!start)
+        return;
+    let [row, col] = start;
+    let direction = directions.right; // Start direction
+    while (row >= 0 && row < matrix.length && col >= 0 && col < matrix[0].length) {
+        let char = matrix[row][col];
+        path += char;
+        // Add letter if it's an uppercase letter
+        if (char >= 'A' && char <= 'Z') {
+            letters += char;
+        }
+        if (char === '+') {
+            // Determine new direction at '+' symbol
+            if (direction !== directions.right && col > 0 && matrix[row][col - 1] === '-') {
+                direction = directions.left;
+            }
+            else if (direction !== directions.left && col < matrix[0].length - 1 && matrix[row][col + 1] === '-') {
+                direction = directions.right;
+            }
+            else if (direction !== directions.down && row > 0 && matrix[row - 1][col] === '|') {
+                direction = directions.up;
+            }
+            else if (direction !== directions.up && row < matrix.length - 1 && matrix[row + 1][col] === '|') {
+                direction = directions.down;
+            }
+        }
+        row += direction[0];
+        col += direction[1];
+        if (char === '|' || char === '-' || char === ' ')
+            continue;
+    }
+}
+traverseMatrix(matrix);
+console.log(`Path: ${path}`);
+console.log(`Letters: ${letters}`);
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # JavaScript_Matrix
+
+This is a JavaScript program that solves a maze traversal challenge. The program takes a matrix as input and traverses the matrix to find the path from the starting point to the end point while collecting uppercase letters encountered.
+
+## Installation
+
+1. Clone the repository: `git clone https://github.com/MinnyS420/JavaScript_Matrix`
+2. Navigate to the project directory: `cd JavaScript_Matrix`
+
+## Usage
+
+1. Open the `index.js` file in your preferred text editor.
+2. Run the program using Node.js: `node index.js`
+3. The program will output the path taken and the uppercase letters encountered during traversal.
+
+## Example
+
+```javascript
+const matrix = [
+  [">", "-", "-", "-", "A", "-", "@", "-", "+"],
+  [" ", " ", " ", " ", " ", " ", " ", " ", "|"],
+  [" ", "+", "-", "U", "-", "+", " ", " ", "C"],
+  [" ", "|", " ", " ", " ", "|", " ", " ", "|"],
+  [" ", "s", " ", " ", " ", "C", "-", "-", "+"],
+];
+```
